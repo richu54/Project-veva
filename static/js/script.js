722f3edbbddc9a031977acce9c716eeee7a1036a
richu54 <richuriz54@gmail.com>
2025-04-26 20:58:35 +0530

    carousal completed! with some changes added on navbar drop-down menu.

--- static/js/script.js
+++ static/js/script.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
 //   acount drop-down
 
 $(document).ready(function() {
-    $("#account-triggers").click(function() {
-      $("#account-dropdowns").slideToggle(500); 
+    $("#account-droptrigger").click(function() {
+      $("#dropdown-account").slideToggle(500); 
       $(".dropdown-icon2").toggleClass("fa-chevron-down fa-chevron-up");
     });
   });
@@ -23,8 +23,97 @@ $(document).ready(function() {
 
 $(document).ready(function(){
     $("#drop-down-mob-view").click(function(){
-        $("#drop-down-sm").slideToggle();
-    })
-})
+        $("#mob-view-drop").slideToggle();
+    });
+});
+
+  
 
 // Navigation bar end --------------------------------------------------------------------------------------
+
+// Carousal start ------------------------------------------------------------------------------------------
+
+$(document).ready(function() {
+  const carousel = new bootstrap.Carousel('#heroCarousel', {
+      interval: 5000, 
+      pause: "hover", 
+      wrap: true
+  });
+  
+  let slideInterval;
+  const startCarousel = () => {
+      slideInterval = setInterval(() => {
+          carousel.next();
+      }, 5000);
+  };
+  
+  const stopCarousel = () => {
+      clearInterval(slideInterval);
+  };
+  
+  startCarousel();
+  
+  $('#heroCarousel').hover(
+      function() {
+          stopCarousel();
+      },
+      function() {
+          startCarousel();
+      }
+  );
+  
+  $(document).on('show.bs.modal', function () {
+      stopCarousel();
+  });
+  
+  $(document).on('hidden.bs.modal', function () {
+      startCarousel();
+  });
+  
+  $('#heroCarousel').on('slide.bs.carousel', function () {
+      $('.carousel-caption').css('opacity', 0);
+  });
+  
+  $('#heroCarousel').on('slid.bs.carousel', function () {
+      $('.carousel-caption').css('opacity', 1);
+      $('.hero-title, .hero-subtitle').css('animation', 'none');
+      setTimeout(() => {
+          $('.hero-title, .hero-subtitle').css('animation', 'fadeInUp 0.8s ease');
+      }, 10);
+  });
+  
+  function adjustCarouselHeight() {
+      var windowHeight = $(window).height();
+      var navHeight = $('nav').outerHeight() || 0;
+      
+      if ($(window).width() < 768) {
+          $('.hero-carousel').css('height', (windowHeight - navHeight) + 'px');
+      } else {
+          $('.hero-carousel').css('height', '');
+      }
+  }
+  
+  adjustCarouselHeight();
+  $(window).resize(adjustCarouselHeight);
+  
+  let touchStartX = 0;
+  let touchEndX = 0;
+  
+  $('#heroCarousel').on('touchstart', function(e) {
+      touchStartX = e.changedTouches[0].screenX;
+  });
+  
+  $('#heroCarousel').on('touchend', function(e) {
+      touchEndX = e.changedTouches[0].screenX;
+      handleSwipe();
+  });
+  
+  function handleSwipe() {
+      if (touchEndX < touchStartX - 50) {
+          carousel.next();
+      }
+      if (touchEndX > touchStartX + 50) {
+          carousel.prev();
+      }
+  }
+});
